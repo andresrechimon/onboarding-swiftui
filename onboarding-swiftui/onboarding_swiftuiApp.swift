31ca51aaa2d20750da15cdea8b011e893ca36346
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-05-07 12:25:54 -0300

    Initial Commit

---
+++ onboarding-swiftui/onboarding_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboarding_swiftuiApp.swift
+//  onboarding-swiftui
+//
+//  Created by Andrés Rechimon on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct onboarding_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
